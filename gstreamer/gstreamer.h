42fd096ce01a2c41ee82363f33329c34f198eb55
Kwon, Yeong-Eon <happylogin@gmail.com>
2016-01-15 13:52:36 +0900

    Initial commit

---
+++ gstreamer/gstreamer.h
@@ -0,0 +1,19 @@
+//
+//  gstreamer-osx.h
+//  gstreamer-osx
+//
+//  Created by Eirny on 1/15/16.
+//  Copyright © 2016 Yet Reader Forge. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for gstreamer.
+FOUNDATION_EXPORT double gstreamerVersionNumber;
+
+//! Project version string for gstreamer.
+FOUNDATION_EXPORT const unsigned char gstreamerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <gstreamer/PublicHeader.h>
+
+
