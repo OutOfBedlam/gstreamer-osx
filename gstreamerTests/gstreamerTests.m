c3dd7410f83d965d1661be0fb4468ecd826ab5e9
Kwon, Yeong-Eon <happylogin@gmail.com>
2016-01-15 18:15:25 +0900

    no message

--- gstreamerTests/gstreamerTests.m
+++ gstreamerTests/gstreamerTests.m
@@ -24,16 +24,20 @@
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+- (void)testExample
+{
+//    NSString *pluginPath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"/usr/lib/gstreamer-1.0"];
+//    NSLog(@"pluginPath = %@", pluginPath);
+//
+//    GstRegistry *registry = gst_registry_get();
 }
 
+#if 0
 - (void)testPerformanceExample {
     // This is an example of a performance test case.
     [self measureBlock:^{
         // Put the code you want to measure the time of here.
     }];
 }
-
+#endif
 @end
